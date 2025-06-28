0f421f824f4b2026b056536b8c16ef9a543293a3
ahmednule <nuleahmed6@gmail.com>
2025-06-28 20:21:47 +0300

    update: changed the design to match the previous design

--- client/src/app/graphql/mutations.ts
+++ client/src/app/graphql/mutations.ts
@@ -1,18 +1,9 @@
 import { gql } from '@apollo/client';
 export const CREATE_USER_MUTATION = gql`
-  mutation CreateFarmer($args: FarmerRegistrationArgs!) {
-    createFarmer(args: $args) {
-      status
-      message
-      token
-      user {
-        id
-        email
-        Fname
-        Lname
-        role
-        emailVerified
-      }
-    }
+mutation CreateFarmer($args: FarmerArgs) {
+  createFarmer(args: $args) {
+    status
+    message
   }
+}
 `;
