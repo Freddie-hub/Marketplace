d62750045b562d33a276830bc446a8339a1357fe
ahmednule <nuleahmed6@gmail.com>
2025-06-29 20:46:33 +0300

    update: configured the toask toncatier application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { ReactNode } from 'react';
 import { ApolloProvider } from './apollo-provider';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ToastContainer } from 'react-toastify';
 
 const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
@@ -9,6 +10,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <ToastContainer/>
         <GoogleOAuthProvider clientId={googleClientId ||  'make-sure-there-is-a-client-id-connected'}>
         <ApolloProvider>{children}</ApolloProvider>
         </GoogleOAuthProvider>
