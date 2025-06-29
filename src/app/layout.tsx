084ee6212adf2a2ea649fea27147f395fb6269e0
ahmednule <nuleahmed6@gmail.com>
2025-06-29 10:45:40 +0300

    feature: added the google oath rovider in the layout file such that authentication would be easy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import './globals.css';
 import { ReactNode } from 'react';
 import { ApolloProvider } from './apollo-provider';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <GoogleOAuthProvider clientId='make-sure-there-is-a-client-id-connected'>
         <ApolloProvider>{children}</ApolloProvider>
+        </GoogleOAuthProvider>
       </body>
     </html>
   );
