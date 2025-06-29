d35cbda08dc0969bee39cbce4376c23439ca5007
ahmednule <nuleahmed6@gmail.com>
2025-06-29 20:33:35 +0300

    update: added the fies in their folder such that it can be easy to maintain the code

---
+++ src/types/WarehouseFormData.ts
@@ -0,0 +1,16 @@
+export interface WarehouseFormData {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    Fname: string;
+    Mname?: string;
+    Lname: string;
+    phone?: string;
+    address?: string;
+    warehouseName: string;
+    warehouseLocation: string;
+    warehouseAddress?: string;
+    warehouseCapacity?: number;
+    warehousePhone?: string;
+    warehouseEmail?: string;
+  }
