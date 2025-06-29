a49619d67caf67558de86fe97fe1a2865568b483
ahmednule <nuleahmed6@gmail.com>
2025-06-29 11:16:48 +0300

    update: added the cleint if to the secret such that it can be easy to track the users in production easily

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,13 @@ import { ReactNode } from 'react';
 import { ApolloProvider } from './apollo-provider';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <GoogleOAuthProvider clientId='make-sure-there-is-a-client-id-connected'>
+        <GoogleOAuthProvider clientId={googleClientId ||  'make-sure-there-is-a-client-id-connected'}>
         <ApolloProvider>{children}</ApolloProvider>
         </GoogleOAuthProvider>
       </body>
