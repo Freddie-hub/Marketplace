2b504ce212ed4a3edfdd332831d7eb6d13760f6e
ahmednule <nuleahmed6@gmail.com>
2025-06-30 22:42:22 +0300

    update: added a toast style such that they cna be seen after they appear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import 'react-toastify/dist/ReactToastify.css'; 
 import { ReactNode } from 'react';
 import { ApolloProvider } from './apollo-provider';
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -10,9 +11,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ToastContainer/>
-        <GoogleOAuthProvider clientId={googleClientId ||  'make-sure-there-is-a-client-id-connected'}>
-        <ApolloProvider>{children}</ApolloProvider>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <GoogleOAuthProvider clientId={googleClientId || 'make-sure-there-is-a-client-id-connected'}>
+          <ApolloProvider>{children}</ApolloProvider>
         </GoogleOAuthProvider>
       </body>
     </html>
